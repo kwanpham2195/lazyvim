ad7f229257ea12cb3f72c83d9042bdb8f2e0545a
quan.pham <quanpm.mta@gmail.com>
2023-07-08 11:26:28 +0700

    chore: add yaml support

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -10,8 +10,6 @@ require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
-    -- import/override with your plugins
-    { import = "plugins" },
     -- import any extras modules here
     -- dap
     { import = "lazyvim.plugins.extras.dap.core" },
@@ -20,7 +18,8 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.tailwind" },
     { import = "lazyvim.plugins.extras.lang.json" },
-    { import = "lazyvim.plugins.extras.lang.go" },
+    { import = "lazyvim.plugins.extras.lang.rust" },
+    { import = "lazyvim.plugins.extras.lang.terraform" },
     -- test
     { import = "lazyvim.plugins.extras.test.core" },
     -- formatting
@@ -36,6 +35,9 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
     { import = "lazyvim.plugins.extras.util.project" },
     { import = "lazyvim.plugins.extras.editor.flash" },
+    -- import/override with your plugins
+    { import = "plugins" },
+    { import = "plugins.lang" },
   },
   defaults = {
     -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -8,7 +8,6 @@ return {
       "stylua",
       "shfmt",
       -- Linter
-      "eslint_d",
       "shellcheck",
       "tflint",
       "yamllint",
