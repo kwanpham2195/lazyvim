0c43ad0ab6560dab4d7b92a0ff4ad9cb3ba35da0
quan.pham <quanpm.mta@gmail.com>
2024-12-23 14:17:27 +0700

    refactor: move to use :LazyExtras

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -16,3 +16,13 @@
 -- */tmp/*,*.so,*.swp,*.zip,**/node_modules/**,**/target/**,**.terraform/**"
 -- ]]
 vim.g.root_spec = { "cwd" }
+
+-- Disable all animations
+vim.g.snacks_animate = false
+
+-- Enable the option to require a Prettier config file
+-- If no prettier config file is found, the formatter will not be used
+vim.g.lazyvim_prettier_needs_config = false
+
+-- Set to false to disable auto format
+vim.g.lazyvim_eslint_auto_format = true
