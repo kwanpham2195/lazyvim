407bf9e648ea74808f410ee108af4cc85ed0d599
quan.pham <quanpm.mta@gmail.com>
2025-02-24 23:02:25 +0700

    add godoc.nvim

---
+++ lua/plugins/go.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "fredrikaverpil/godoc.nvim",
+    version = "*",
+    dependencies = {
+      { "folke/snacks.nvim" },
+      {
+        "nvim-treesitter/nvim-treesitter",
+        opts = {
+          ensure_installed = { "go" },
+        },
+      },
+    },
+    build = "go install github.com/lotusirous/gostdsym/stdsym@latest",
+    cmd = { "GoDoc" },
+    opts = {},
+  },
+}
