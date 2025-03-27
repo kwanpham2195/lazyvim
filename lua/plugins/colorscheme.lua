d47e0dea737a798f68d9400b735f78b958120a86
quan.pham <quanpm.mta@gmail.com>
2025-03-27 14:55:26 +0700

    update color scheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -34,7 +34,7 @@ return {
         light = "latte",
         dark = "macchiato",
       },
-      term_colors = true, -- sets terminal colors (e.g. `g:terminal_color_0`)
+      term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
       transparent_background = false, -- disables setting the background color.
       show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
       dim_inactive = {
@@ -56,7 +56,34 @@ return {
         types = {},
       },
       color_overrides = {
-        latte = {},
+        latte = {
+          rosewater = "#cc7983",
+          flamingo = "#bb5d60",
+          pink = "#d54597",
+          mauve = "#a65fd5",
+          red = "#b7242f",
+          maroon = "#db3e68",
+          peach = "#e46f2a",
+          yellow = "#bc8705",
+          green = "#1a8e32",
+          teal = "#00a390",
+          sky = "#089ec0",
+          sapphire = "#0ea0a0",
+          blue = "#017bca",
+          lavender = "#8584f7",
+          text = "#444444",
+          subtext1 = "#555555",
+          subtext0 = "#666666",
+          overlay2 = "#777777",
+          overlay1 = "#888888",
+          overlay0 = "#999999",
+          surface2 = "#aaaaaa",
+          surface1 = "#dddddd",
+          surface0 = "#cccccc",
+          base = "#ffffff",
+          mantle = "#eeeeee",
+          crust = "#dddddd",
+        },
         frappe = {},
         macchiato = {},
         mocha = {},
@@ -74,6 +101,7 @@ return {
         headlines = true,
         illuminate = true,
         indent_blankline = { enabled = true },
+        fzf = true,
         leap = true,
         lsp_trouble = true,
         mason = true,
